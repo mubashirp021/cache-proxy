5ece733a4764ea01c967745177ac29660a24068d
Your Nameclear <you@example.com>
2025-09-05 11:57:50 +0530

    add CLI

--- src/cli/ProxyCLI.java
+++ src/cli/ProxyCLI.java
@@ -1,5 +1,20 @@
 package cli;
 
+import server.ProxyServer;
+import utils.Config;
+
 public class ProxyCLI {
-    
+    public static void main(String[] args) {
+        int port = Config.DEFAULT_PORT;
+        if (args.length > 0) {
+            try {
+                port = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid port, using default " + port);
+            }
+        }
+
+        ProxyServer server = new ProxyServer(port);
+        server.start();
+    }
 }
